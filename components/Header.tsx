4bcdf44c39517c385b5445e635230c737e0f8520
nannipy <pernazza.1936239@studenti.uniroma1.it>
2025-12-29 14:37:41 +0100

    definitivo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,10 +10,7 @@ const Header: React.FC<HeaderProps> = ({ onReset }) => {
     <header className="pt-12 pb-12 px-6 text-center">
       <div className="flex flex-col items-center max-w-4xl mx-auto">
         <div className="flex items-center gap-3 mb-4">
-          <div className="w-10 h-10 bg-google-blue rounded-xl flex items-center justify-center text-white shadow-sm">
-            <i className="fas fa-utensils"></i>
-          </div>
-          <h1 className="text-3xl font-medium tracking-tight">Capodanno 2025</h1>
+          <h1 className="text-3xl font-medium tracking-tight">🍾 Capodanno 2025</h1>
         </div>
         
       </div>
